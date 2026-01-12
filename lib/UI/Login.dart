8cf5ae67ae490c165bd4413b41a94b88244b632a
christodharma <christodharma@gmail.com>
2026-01-12 09:02:15 +0700

    WIP: Login page UI

--- lib/UI/Login.dart
+++ lib/UI/Login.dart
@@ -8,12 +8,63 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  final UsernameController = TextEditingController();
-  final PasswordController = TextEditingController();
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+  bool _passwordVisibility = false;
+
+  void loginSubmit() {
+    var username = usernameController.text;
+    var password = passwordController.text;
+    // TODO login submission
+  }
+
+  void togglePasswordVisibility() {
+    setState(() {
+      _passwordVisibility = !_passwordVisibility;
+    });
+  }
+
+  @override
+  void dispose() {
+    usernameController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      body: Padding(
+        padding: .directional(start: 16, end: 16),
+        child: Column(
+          mainAxisAlignment: .center,
+          spacing: 16,
+          children: [
+            TextFormField(
+              decoration: InputDecoration(labelText: "Username"),
+              controller: usernameController,
+              // TODO add validator
+            ),
+            TextFormField(
+              // TODO add validator
+              obscureText: _passwordVisibility,
+              decoration: InputDecoration(
+                labelText: "Password",
+                suffixIcon: IconButton(
+                  onPressed: togglePasswordVisibility,
+                  icon: Icon(
+                    _passwordVisibility
+                        ? Icons.visibility
+                        : Icons.visibility_off,
+                  ),
+                ),
+              ),
+              controller: passwordController,
+            ),
+            ElevatedButton(onPressed: loginSubmit, child: Text("Login")),
+          ],
+        ),
+      ),
+    );
   }
 }
