29c19070ce91ff1c4c4801da2ba1e8d09a05a1cb
christodharma <christodharma@gmail.com>
2026-01-12 09:21:30 +0700

    WIP: Settings page UI

--- lib/UI/Settings.dart
+++ lib/UI/Settings.dart
@@ -8,9 +8,39 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+  void saveTransactions() {
+    // TODO simpan daftar transaksi
+  }
+
+  void sendTransactions() {
+    // TODO send daftar transaksi
+  }
+
+  void logout() {
+    // TODO keluar
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: .start,
+        crossAxisAlignment: .stretch,
+        children: [
+          ElevatedButton.icon(
+            onPressed: saveTransactions,
+            label: Text("Simpan daftar transaksi"),
+          ),
+          ElevatedButton.icon(
+            onPressed: sendTransactions,
+            label: Text("Kirim daftar transaksi"),
+          ),
+          ElevatedButton.icon(
+            onPressed: logout,
+            label: Text("Keluar", style: TextStyle(color: Colors.red)),
+          ),
+        ],
+      ),
+    );
   }
 }
