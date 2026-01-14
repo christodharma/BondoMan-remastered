4034f707d50bb04f817d4827e661623f4c424d9f
christodharma <christodharma@gmail.com>
2026-01-14 12:13:44 +0700

    fix: standalone ui

--- lib/ui/transaction_input.dart
+++ lib/ui/transaction_input.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_project_1/data/transaction/transaction.dart';
 
 class TransactionInput extends StatefulWidget {
   const TransactionInput({super.key});
@@ -12,29 +11,22 @@ class TransactionInput extends StatefulWidget {
 class _TransactionInputState extends State<TransactionInput> {
   final nameController = TextEditingController();
   final nominalController = TextEditingController();
-  Category? category;
+  int? category;
   final locationController = TextEditingController();
   DateTime? dateTime;
 
-  Transaction toTransaction() {
-    return Transaction.withNoId(
-      name: nameController.text,
-      nominal: double.tryParse(nominalController.text),
-      category: category,
-      location: locationController.text,
-      dateTime: dateTime,
+  void _pressPickDate() async {
+    var pickedDate = await showDatePicker(
+      context: context,
+      initialEntryMode: DatePickerEntryMode.calendarOnly,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2024),
+      lastDate: DateTime.now(),
     );
+    setState(() {
+      dateTime = pickedDate;
+    });
   }
-
-  void fromTransaction(Transaction source) {
-    nameController.text = source.name ?? "";
-    nominalController.text = source.nominal.toString();
-    category = source.category;
-    locationController.text = source.location ?? "";
-    dateTime = source.dateTime;
-  }
-
-  //TODO implement initState
 
   @override
   void dispose() {
@@ -48,87 +40,94 @@ class _TransactionInputState extends State<TransactionInput> {
   Widget build(BuildContext context) {
     //TODO add validators
     return Scaffold(
-      body: Padding(
+      body: ListView(
         padding: const EdgeInsets.all(16),
-        child: Align(
-          alignment: .topCenter,
-          child: Column(
-            spacing: 16,
-            children: [
-              // input item name
-              TextFormField(
-                controller: nameController,
-                decoration: const InputDecoration(
-                  labelText: "Item Name",
-                  border: OutlineInputBorder(),
-                ),
+        children: [
+          // input item name
+          Padding(
+            padding: .directional(top: 8, bottom: 8),
+            child: TextFormField(
+              controller: nameController,
+              decoration: const InputDecoration(
+                labelText: "Item Name",
+                border: OutlineInputBorder(),
               ),
-              // input nominal
-              TextFormField(
-                keyboardType: .number,
-                inputFormatters: <TextInputFormatter>[
-                  FilteringTextInputFormatter.digitsOnly,
-                ],
-                controller: nominalController,
-                decoration: const InputDecoration(
-                  labelText: "Price",
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              // input category
-              RadioGroup<Category>(
-                groupValue: category,
-                onChanged: (Category? value) {
-                  setState(() {
-                    category = value;
-                  });
-                },
-                child: Column(
-                  children: [
-                    Text("Category"),
-                    ListTile(
-                      title: const Text("Pengeluaran"),
-                      leading: Radio<Category>(
-                        value: Category.send,
-                        activeColor: Colors.red,
-                      ),
-                    ),
-                    ListTile(
-                      title: const Text("Pemasukan"),
-                      leading: Radio<Category>(
-                        value: Category.receive,
-                        activeColor: Colors.green,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              // input date
-              ElevatedButton.icon(
-                icon: const Icon(Icons.calendar_today),
-                onPressed: () async {
-                  var pickedDate = await showDatePicker(
-                    context: context,
-                    initialEntryMode: DatePickerEntryMode.calendarOnly,
-                    initialDate: DateTime.now(),
-                    firstDate: DateTime(2024),
-                    lastDate: DateTime.now(),
-                  );
-                  setState(() {
-                    dateTime = pickedDate;
-                  });
-                },
-                label: const Text('Pick a date'),
-              ),
-              ElevatedButton.icon(
-                onPressed: () {
-                  // TODO add callback (run validation to input obj!)
-                },
-                label: Text("Submit"),
-              ),
-            ],
+            ),
           ),
-        ),
+          // input nominal
+          Padding(
+            padding: .directional(top: 8, bottom: 8),
+            child: TextFormField(
+              keyboardType: .number,
+              inputFormatters: <TextInputFormatter>[
+                FilteringTextInputFormatter.digitsOnly,
+              ],
+              controller: nominalController,
+              decoration: const InputDecoration(
+                labelText: "Price",
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+          // input category
+          Padding(
+            padding: .directional(top: 8, bottom: 8),
+            child: RadioGroup<int>(
+              groupValue: category,
+              onChanged: (int? value) {
+                setState(() {
+                  category = value;
+                });
+              },
+              child: Column(
+                children: [
+                  Text("Category"),
+                  ListTile(
+                    title: const Text("Pengeluaran"),
+                    leading: Radio<int>(
+                      value: 0,
+                      activeColor: Colors.red,
+                    ),
+                  ),
+                  ListTile(
+                    title: const Text("Pemasukan"),
+                    leading: Radio<int>(
+                      value: 1,
+                      activeColor: Colors.green,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          // input date
+          Padding(
+            padding: .directional(top: 8, bottom: 8),
+            child: Row(
+              mainAxisAlignment: .spaceBetween,
+              children: [
+                Text(
+                  dateTime != null
+                      ? "${dateTime!.day}/${dateTime!.month}/${dateTime!.year}"
+                      : "Pick a date",
+                ),
+                OutlinedButton.icon(
+                  onPressed: _pressPickDate,
+                  label: Text("Pick date"),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: .directional(top: 8, bottom: 8),
+            child: ElevatedButton.icon(
+              onPressed: () {
+                // TODO on submit
+              },
+              label: Text("Submit"),
+            ),
+          ),
+        ],
       ),
     );
   }
