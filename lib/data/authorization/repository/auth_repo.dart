668a38c09dad6109d154cda4c6aac93135291943
christodharma <christodharma@gmail.com>
2026-01-23 16:03:46 +0700

    feat: session object, auth repo with session refactor: test directory

--- lib/data/authorization/repository/auth_repo.dart
+++ lib/data/authorization/repository/auth_repo.dart
@@ -1,15 +1,20 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_project_1/data/authorization/authorization_result.dart';
+import 'package:flutter_project_1/data/authorization/authorization_credential.dart';
 import 'package:flutter_project_1/data/authorization/service/i_auth.dart';
-import 'package:flutter_project_1/data/user.dart';
+import 'package:flutter_project_1/data/authorization/session.dart';
 
 abstract interface class IAuthorizationRepository {
-  Future<AuthorizationResult> login({
-    required String username,
-    required String key,
-  });
+  Session get getSession;
 
-  // Future<bool> logout(User user);
+  Stream<AuthState> get onStateChange;
+
+  void setState(AuthState state);
+
+  Future<void> authorize(AuthCredential cred);
+
+  Future<void> removeSession();
 }
 
 class AuthorizationRepository extends ChangeNotifier
@@ -18,23 +23,52 @@ class AuthorizationRepository extends ChangeNotifier
 
   AuthorizationRepository(this.service);
 
-  User? _currentUser;
+  Session _session = .nullSession;
+  final StreamController<AuthState> _streamController =
+      StreamController<AuthState>.broadcast();
 
   @override
-  Future<AuthorizationResult> login({
-    required String username,
-    required String key,
-  }) async {
-    if (_currentUser != null) {
-      return AuthorizationResult(
-        success: true,
-        reason: "login with active session",
-        user: _currentUser,
-      );
+  Session get getSession => _session;
+
+  @override
+  Stream<AuthState> get onStateChange => _streamController.stream;
+
+  @override
+  void setState(AuthState state) {
+    if (_session == .nullSession) {
+      _session = Session("", .nullAuthCredential, state);
+    } else {
+      _session.state = state;
     }
 
-    var result = await service.authorize(username: username, key: key);
-    _currentUser = result.user;
-    return result;
+    _streamController.add(state);
+  }
+
+  @override
+  Future<void> authorize(AuthCredential cred) async {
+    var result = await service.signIn(username: cred.username, key: cred.key);
+    if (result.success){
+      _setCredential(cred);
+      setState(.authenticated);
+    }
+    else {
+      // TODO add AuthorizationResult handles
+      return;
+    }
+  }
+
+  @override
+  Future<void> removeSession() async {
+    await service.signOut();
+    _session = .nullSession;
+    setState(_session.state); // state = guest
+  }
+
+  void _setCredential(AuthCredential cred){
+    if (_session == .nullSession){
+      _session = Session("", cred, .guest);
+    } else {
+      _session.cred = cred;
+    }
   }
 }
