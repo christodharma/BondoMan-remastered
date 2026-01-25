64f347e37789d7fade6447d6079459aef183b543
christodharma <christodharma@gmail.com>
2026-01-25 21:30:32 +0700

    fix: login view model

--- lib/ui/login/login_viewmodel.dart
+++ lib/ui/login/login_viewmodel.dart
@@ -1,23 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_project_1/data/authorization/authorization_credential.dart';
-import 'package:flutter_project_1/data/authorization/repository/auth_repo.dart';
-import 'package:flutter_project_1/data/authorization/session.dart';
+import 'package:flutter_project_1/data/authorization/repository/i_auth_repo.dart';
 
 class LoginViewModel extends ChangeNotifier {
   final IAuthorizationRepository _auth;
 
-  LoginViewModel(this._auth) {
-    _auth.onStateChange.listen((newState) {
-      switch (newState) {
-        case AuthState.authenticated:
-          // TODO: Handle this case.
-          throw UnimplementedError();
-        case AuthState.guest:
-          // TODO: Handle this case.
-          throw UnimplementedError();
-      }
-    });
-  }
+  LoginViewModel(this._auth);
 
   Future<bool> submitLogin({
     required String username,
